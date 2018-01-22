ccb0eebb2f512dc7f37e47df65345afcc84e27ea
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-01-22 07:45:51 +0000

    module working example

--- modules-example/modules.tf
+++ modules-example/modules.tf
@@ -3,6 +3,12 @@ module "consul" {
   key_name = "${aws_key_pair.mykey.key_name}"
   key_path = "${var.PATH_TO_PRIVATE_KEY}"
   region = "${var.AWS_REGION}"
+  vpc_id = "${aws_vpc.main.id}"
+  subnets {
+    "0" = "${aws_subnet.main-public-1.id}"
+    "1" = "${aws_subnet.main-public-2.id}"
+    "2" = "${aws_subnet.main-public-3.id}"
+  }
 }
 output "consul-output" {
   value = "${module.consul.server_address}"
--- modules-example/vars.tf
+++ modules-example/vars.tf
@@ -1,9 +1,5 @@
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
-}
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
-}
+variable "PATH_TO_PRIVATE_KEY" {}
+variable "PATH_TO_PUBLIC_KEY" {}
